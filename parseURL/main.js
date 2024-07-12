f3246c3b495feb73e8656543f85f5cbfac50f012
Wilmer Blanco <wilmervzla2000@gmail.com>
2024-07-12 16:33:58 -0300

    Agregado nueva imagenes y funciones

--- parseURL/main.js
+++ parseURL/main.js
@@ -1,48 +1,49 @@
-import {UrlParser} from './classes/url-parse.class.js';
-const inputURL = document.getElementById('input-url');
-const inputFormat = document.getElementById('input-format');
-const jsonViewer = document.getElementById('json-viewer');
-const urlList = document.getElementById('url-list');
-const parseButton = document.getElementById('parse-url-button');
-const urlParser = new UrlParser();
+(async () => {
+    const {UrlParser} = await import('./classes/url-parse.class.js');
+    const inputURL = document.getElementById('input-url');
+    const inputFormat = document.getElementById('input-format');
+    const jsonViewer = document.getElementById('json-viewer');
+    const urlList = document.getElementById('url-list');
+    const parseButton = document.getElementById('parse-url-button');
+    const urlParser = new UrlParser();
 
-function setJSONViewer(urlParsed) {
-    jsonViewer.textContent = JSON.stringify(urlParsed, null, 2);
-}
+    function setJSONViewer(urlParsed) {
+        jsonViewer.textContent = JSON.stringify(urlParsed, null, 2);
+    }
 
-function setListViewer(urlParsed) {
-    urlList.innerHTML = Object.keys(urlParsed).map((k) => {
-        return `<li>
+    function setListViewer(urlParsed) {
+        urlList.innerHTML = Object.keys(urlParsed).map((k) => {
+            return `<li>
                     <div class="d-flex">
                         <div class="flex-grow-1">
                             <b class="d-block">${k}</b>
                         </div>
                         <div class="flex-grow-1"></div>
-                        
                         <span class="list-block">${urlParsed[k]}</span>
                     </div>
                 </li>`
-    }).join('');
-}
+        }).join('');
+    }
 
-function setEventListeners() {
-    inputURL.addEventListener('keyup', (event) => {
-        urlParser.urlToParse = event.target.value ?? '';
-    });
-    inputFormat.addEventListener('keyup', (event) => {
-        urlParser.formatToUse = event.target.value;
-    });
-    parseButton.addEventListener('click', () => {
-        const urlParsed = urlParser.parse();
-        if (urlParsed) {
-            setJSONViewer(urlParsed);
-            setListViewer(urlParsed);
-        }
-    });
-}
+    function setEventListeners() {
+        inputURL.addEventListener('keyup', (event) => {
+            urlParser.urlToParse = event.target.value ?? '';
+        });
+        inputFormat.addEventListener('keyup', (event) => {
+            urlParser.formatToUse = event.target.value;
+        });
+        parseButton.addEventListener('click', () => {
+            const urlParsed = urlParser.parse();
+            if (urlParsed) {
+                setJSONViewer(urlParsed);
+                setListViewer(urlParsed);
+            }
+        });
+    }
 
-function main() {
-    setEventListeners();
-}
+    function main() {
+        setEventListeners();
+    }
 
-main();
+    main();
+})();
